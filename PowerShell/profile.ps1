bb320427a6c0ca37f7eb4c6d3c6a7d2a1661fd4c
Yue JIN <yjin@nustarnuclear.com>
2022-12-27 14:03:17 +0800

    🎉 Init repo

---
+++ PowerShell/profile.ps1
@@ -0,0 +1,17 @@
+function set-proxy{
+    $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
+}
+
+function cdlnk($target)
+{
+    if($target.EndsWith(".lnk"))
+    {
+        $sh = new-object -com wscript.shell
+        $fullpath = resolve-path $target
+        $targetpath = $sh.CreateShortcut($fullpath).TargetPath
+        set-location $targetpath
+    }
+    else {
+        set-location $target
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# KingYue's dotfiles
