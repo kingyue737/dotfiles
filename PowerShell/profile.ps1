85af871f2b98a0648dace8109027b6d1b30ead59
Yue JIN <yjin@nustarnuclear.com>
2022-12-28 14:48:20 +0800

    ✨ Add thefuck

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,11 +1,9 @@
-function set-proxy{
-    $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
+function set-proxy {
+    $Env:http_proxy = "http://127.0.0.1:7890"; $Env:https_proxy = "http://127.0.0.1:7890"
 }
 
-function cdlnk($target)
-{
-    if($target.EndsWith(".lnk"))
-    {
+function cdlnk($target) {
+    if ($target.EndsWith(".lnk")) {
         $sh = new-object -com wscript.shell
         $fullpath = resolve-path $target
         $targetpath = $sh.CreateShortcut($fullpath).TargetPath
@@ -15,3 +13,6 @@ function cdlnk($target)
         set-location $target
     }
 }
+
+$env:PYTHONIOENCODING = "utf-8"
+Invoke-Expression "$(thefuck --alias)"
