96344747ed4f9c467c6677a2913b7f54b23b9a68
Yue JIN <yuejin13@qq.com>
2023-01-31 20:27:54 +0800

    🚀 Symlink all config files in repo

---
+++ setup.ps1
@@ -0,0 +1,18 @@
+git config --global user.name "Yue JIN"
+# git config --global user.email "yuejin13@qq.com"
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy http://127.0.0.1:7890
+
+$paths = @(
+    "Documents/PowerShell/Microsoft.PowerShell_profile.ps1", 
+    "Documents/PowerShell/profile.ps1", 
+    "Documents/PowerShell/powershell.config.json",
+    ".docker/daemon.json",
+    "pip/pip.ini",
+    ".wslconfig",
+    ".npmrc"
+)
+
+foreach ($path in $paths) {
+    New-Item -Force -Path "~/$path" -ItemType SymbolicLink -Value (Get-Item "./$path").FullName
+}
--- symlinks.ps1
+++
@@ -1,1 +0,0 @@
-New-Item -Path ~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\PowerShell\Microsoft.PowerShell_profile.ps1").FullName
