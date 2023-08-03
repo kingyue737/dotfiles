0d9144a77b1c30bf16d6ee71107cc8dca4fef9be
Yue JIN <yjin@nustarnuclear.com>
2023-08-03 11:37:36 +0800

    🔨 Update pdm installing script uri

--- install-pdm.ps1
+++ install-pdm.ps1
@@ -1,11 +1,1 @@
-# (Invoke-WebRequest -Uri https://pdm.fming.dev/dev/install-pdm.py -UseBasicParsing).Content | python -
-
-$url = 'https://pdm.fming.dev/dev/install-pdm.py'
-[System.Net.HttpWebRequest]$req = [System.Net.WebRequest]::Create($url) -as [System.Net.HttpWebRequest]
-[System.Net.HttpWebResponse]$resp = $req.getResponse()
-$respStream = $resp.getResponseStream()
-$sr = new-object IO.StreamReader($respStream)
-$result = $sr.ReadToEnd()
-$resp.Close()
-
-Write-Output $result | python -
+(Invoke-WebRequest -Uri https://pdm.fming.dev/install-pdm.py -UseBasicParsing).Content | python -
