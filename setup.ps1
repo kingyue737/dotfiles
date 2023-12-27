c4ea06bf75ffae585d7c4d15b5c02d6afc58b942
Yue JIN <yjin@nustarnuclear.com>
2023-12-27 16:39:00 +0800

    🔧 Git auto setup remote

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,7 @@ git config --global user.name "Yue JIN"
 # git config --global user.email "yuejin13@qq.com"
 git config --global http.proxy http://127.0.0.1:7890
 git config --global https.proxy http://127.0.0.1:7890
+git config --global push.autoSetupRemote true
 
 $paths = @(
     "Documents/PowerShell/Microsoft.PowerShell_profile.ps1", 
