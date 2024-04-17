205591f462743d87e8c9b0824b657c34ced047ea
Yue JIN <yjin@nustarnuclear.com>
2024-04-17 14:10:02 +0800

    ➕ Add PowerToys CommandNotFound module

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
 oh-my-posh init pwsh --config "$env:USERPROFILE/dotfiles/omp.json" | Invoke-Expression
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
